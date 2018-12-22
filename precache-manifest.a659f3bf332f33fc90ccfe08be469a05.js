1305a252d0af3a8b9c89008053616dd954b0160f
Garrett Fermo <garrettcfermo@gmail.com>
2018-12-21 23:18:17 -0800

    Updates

---
+++ precache-manifest.a659f3bf332f33fc90ccfe08be469a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b119ab33f3dc58ebd52",
+    "url": "/garrettcfermo/PokemonClickyGameReact/static/js/runtime~main.6b119ab3.js"
+  },
+  {
+    "revision": "f56e0bbd80df1eb76a9a",
+    "url": "/garrettcfermo/PokemonClickyGameReact/static/js/main.f56e0bbd.chunk.js"
+  },
+  {
+    "revision": "6f34c7035410da448ec2",
+    "url": "/garrettcfermo/PokemonClickyGameReact/static/js/1.6f34c703.chunk.js"
+  },
+  {
+    "revision": "f56e0bbd80df1eb76a9a",
+    "url": "/garrettcfermo/PokemonClickyGameReact/static/css/main.7777b216.chunk.css"
+  },
+  {
+    "revision": "91ceb0e8d34633df059ff39fc836e244",
+    "url": "/garrettcfermo/PokemonClickyGameReact/index.html"
+  }
+];
